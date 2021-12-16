eb066911234f508f8572b5fcfc5d140a6b930444
Shahzeb Imran <s.imran@tuta.io>
2021-12-16 15:26:01 +1000

    formatting

--- src/jvc_projector/jvccommands.py
+++ src/jvc_projector/jvccommands.py
@@ -1,12 +1,17 @@
+"""Commands for the jvc_projector library."""
+
 from enum import Enum
+
 # Headers
-OPR = b"!\x89\x01" # operation
-REF = b"?\x89\x01" # reference
-RES = b"@\x89\x01" # response
+OPR = b"!\x89\x01"  # operation
+REF = b"?\x89\x01"  # reference
+RES = b"@\x89\x01"  # response
+PACK = b"\x06\x89\x01"  # projector ack
+
 
 class Commands(Enum):
     # power commands
-    power_on  = OPR + b'PW1\n'
+    power_on = OPR + b"PW1\n"
     power_off = OPR + b"PW0\n"
 
     # lens memory commands
@@ -21,75 +26,75 @@ class Commands(Enum):
     hdmi2 = OPR + b"IP7\n"
 
     # picture mode commands
-    pm_cinema  = OPR + b"PMPM01\n"
-    pm_hdr     = OPR + b"PMPM04\n"
+    pm_cinema = OPR + b"PMPM01\n"
+    pm_hdr = OPR + b"PMPM04\n"
     pm_natural = OPR + b"PMPM03\n"
-    pm_film    = OPR + b"PMPM00\n"
-    pm_THX     = OPR + b"PMPM06\n"
-    pm_user1   = OPR + b"PMPM0C\n"
-    pm_user2   = OPR + b"PMPM0D\n"
-    pm_user3   = OPR + b"PMPM0E\n"
-    pm_user4   = OPR + b"PMPM0F\n"
-    pm_user5   = OPR + b"PMPM10\n"
-    pm_user6   = OPR + b"PMPM11\n"
-    pm_hlg     = OPR + b"PMPM14\n"
+    pm_film = OPR + b"PMPM00\n"
+    pm_THX = OPR + b"PMPM06\n"
+    pm_user1 = OPR + b"PMPM0C\n"
+    pm_user2 = OPR + b"PMPM0D\n"
+    pm_user3 = OPR + b"PMPM0E\n"
+    pm_user4 = OPR + b"PMPM0F\n"
+    pm_user5 = OPR + b"PMPM10\n"
+    pm_user6 = OPR + b"PMPM11\n"
+    pm_hlg = OPR + b"PMPM14\n"
 
     # low latency enable/disable
-    pm_low_latency_enable  = OPR + b"PMLL1\n"
+    pm_low_latency_enable = OPR + b"PMLL1\n"
     pm_low_latency_disable = OPR + b"PMLL0\n"
 
     # mask commands
-    mask_off     = OPR + b"ISMA2\n"
+    mask_off = OPR + b"ISMA2\n"
     mask_custom1 = OPR + b"ISMA0\n"
     mask_custom2 = OPR + b"ISMA1\n"
     mask_custom3 = OPR + b"ISMA3\n"
 
-    #lamp commands
+    # lamp commands
     lamp_high = OPR + b"PMLP1\n"
-    lamp_low  = OPR + b"PMLP0\n"
+    lamp_low = OPR + b"PMLP0\n"
 
-    #menu controls
-    menu       = OPR + b"RC732E\n"
-    menu_down  = OPR + b"RC7302\n"
-    menu_left  = OPR + b"RC7336\n"
+    # menu controls
+    menu = OPR + b"RC732E\n"
+    menu_down = OPR + b"RC7302\n"
+    menu_left = OPR + b"RC7336\n"
     menu_right = OPR + b"RC7334\n"
-    menu_up    = OPR + b"RC7301\n"
-    menu_ok    = OPR + b"RC732F\n"
-    menu_back  = OPR + b"RC7303\n"
+    menu_up = OPR + b"RC7301\n"
+    menu_ok = OPR + b"RC732F\n"
+    menu_back = OPR + b"RC7303\n"
 
-    #Lens Aperture commands
+    # Lens Aperture commands
     aperture_off = OPR + b"PMDI0\n"
     aperture_auto1 = OPR + b"PMDI1\n"
     aperture_auto2 = OPR + b"PMDI2\n"
 
-    #Anamorphic commands
+    # Anamorphic commands
     anamorphic_off = OPR + b"INVS0\n"
     anamorphic_a = OPR + b"INVS1\n"
     anamorphic_b = OPR + b"INVS2\n"
     anamorphic_c = OPR + b"INVS3\n"
 
-    #MAC Address query
+    # MAC Address query
     get_mac = REF + b"LSMA\n"
 
-    #model query
+    # model query
     model = REF + b"MD\n"
 
     # power status query commands
-    power_status   = REF + b"PW\n"
+    power_status = REF + b"PW\n"
     current_input = REF + b"IP\n"
     signal_active = REF + b"SC\n"
 
 
 class Responses(Enum):
-    standby   = RES + b"PW0\n"
-    cooling   = RES + b"PW2\n"
+    standby = RES + b"PW0\n"
+    cooling = RES + b"PW2\n"
     emergency = RES + b"PW4\n"
 
     # on some projectors like the DLA-X5900, the status
     # is returned as the "reserved" on below when the
     # projector lamp is warming up and "lamp_on" when
     # the lamp is on
-    lamp_on  = RES + b"PW1\n"
+    lamp_on = RES + b"PW1\n"
     reserved = RES + b"PW3\n"
 
     hdmi1 = RES + b"IP6\n"
--- src/jvc_projector/jvcprojector.py
+++ src/jvc_projector/jvcprojector.py
@@ -16,12 +16,9 @@ Example:
 """
 
 import asyncio
-from .jvccommands import Commands, Responses
-from aiolimiter import AsyncLimiter
+from .jvccommands import Commands, Responses, PACK
 import time
-import logging
 
-_LOGGER = logging.getLogger(__name__)
 
 class JVCProjectorClient:
     """This class handles sending and receiving information from the projector.
@@ -51,17 +48,19 @@ class JVCProjectorClient:
     """
 
     def __init__(
-            self,
-            host: str,
-            port: int = None,
-            delay_seconds: float = None,
-            connect_timeout_seconds: int = None,
+        self,
+        host: str,
+        port: int = None,
+        delay_seconds: float = None,
+        connect_timeout_seconds: int = None,
     ) -> None:
 
         self.host: str = host
         self.port: int = port if port else 20554
         self._delay_seconds: float = delay_seconds if delay_seconds else 0.7
-        self._connect_timeout_seconds: int = connect_timeout_seconds if connect_timeout_seconds else 10
+        self._connect_timeout_seconds: int = (
+            connect_timeout_seconds if connect_timeout_seconds else 10
+        )
         self._last_command_time = time.time()
         self._lock = asyncio.Lock()
 
@@ -76,23 +75,29 @@ class JVCProjectorClient:
                 If the first byte of the command signifies an operation (b'!'),
                 the response will be empty, b''
         """
-        JVC_GREETING = b'PJ_OK'
-        JVC_REQ = b'PJREQ'
-        JVC_ACK = b'PJACK'
+        JVC_GREETING = b"PJ_OK"
+        JVC_REQ = b"PJREQ"
+        JVC_ACK = b"PJACK"
 
         await self._throttle()
 
         try:
-            reader, writer = await asyncio.wait_for(asyncio.open_connection(self.host, self.port), timeout=self._connect_timeout_seconds)
+            reader, writer = await asyncio.wait_for(
+                asyncio.open_connection(self.host, self.port),
+                timeout=self._connect_timeout_seconds,
+            )
         except ConnectionRefusedError:
-            # print("error")
-            raise JVCCannotConnectError("Could not connect to the projector. Check the Hostname/IP and ensure that 'Control4' is turned off in the network settings.")
+            raise JVCCannotConnectError(
+                "Could not connect to the projector. Check the Hostname/IP and ensure that 'Control4' is turned off in the network settings."
+            )
 
         # 3 step handshake:
         # Projector sends PJ_OK, client sends PJREQ, projector replies with PJACK
         # first, after connecting, see if we receive PJ_OK. If not, raise exception
         if await reader.read(len(JVC_GREETING)) != JVC_GREETING:
-            raise JVCHandshakeError("Projector did not reply with correct PJ_OK greeting.")
+            raise JVCHandshakeError(
+                "Projector did not reply with correct PJ_OK greeting."
+            )
 
         # Now send PJREQ.
         writer.write(JVC_REQ)
@@ -105,11 +110,11 @@ class JVCProjectorClient:
         # TODO: implement timeout for this
         writer.write(operation)
 
-        ack = b"\x06\x89\x01" + operation[3:5] + b"\x0A"
+        ack = PACK + operation[3:5] + b"\n"
         ACK = await reader.read(len(ack))
 
-        result = b''
-        wait_for_response = True if operation[0:1] == b'?' else False
+        result = b""
+        wait_for_response = True if operation[0:1] == b"?" else False
         if ACK == ack:
             if wait_for_response:
                 message = await reader.read(1024)
@@ -119,18 +124,14 @@ class JVCProjectorClient:
 
         writer.close()
 
-        # loop = asyncio.get_event_loop()
-        # print(loop.time(), self._last_command_time, " ", loop.time() - self._last_command_time)
         async with self._lock:
             self._last_command_time = time.time()
-        # print("just ran", self._last_command_time)
 
         return result
 
     def _send_command(self, operation):
         """Call async_send command synchronously"""
         return asyncio.run(self._async_send_command(operation))
-
 
     async def _throttle(self):
         """Throttles the comminication."""
@@ -139,7 +140,6 @@ class JVCProjectorClient:
             return
 
         delta = time.time() - self._last_command_time
-        _LOGGER.error("Waiting for %f seconds, default delay is: %f " % (self._delay_seconds - delta, self._delay_seconds))
         if self._delay_seconds > delta:
             return await asyncio.sleep(self._delay_seconds - delta)
         return
@@ -170,7 +170,9 @@ class JVCProjectorClient:
         """
         try:
             if not await self.async_is_lamp_on():
-                raise JVCPoweredOffError("Can't send this command, the projector is not powered on.")
+                raise JVCPoweredOffError(
+                    "Can't send this command, the projector is not powered on."
+                )
             await self._async_send_command(Commands[command_string].value)
         except ValueError:
             raise JVCCommandNotFoundError("The requested command does not exist.")
@@ -193,7 +195,7 @@ class JVCProjectorClient:
         """Get the MAC address of the projector."""
         mac = await self._async_send_command(Commands.get_mac.value)
         if mac:
-            return mac[5:-1].decode("ascii") # skip the header and end
+            return mac[5:-1].decode("ascii")  # skip the header and end
         else:
             raise JVCCommunicationError("Unexpected response for get_mac()")
 
@@ -205,7 +207,7 @@ class JVCProjectorClient:
         """Get the model string of the projector."""
         model = await self._async_send_command(Commands.model.value)
         if model:
-            return model[5:-1].decode("ascii") # skip the header and end
+            return model[5:-1].decode("ascii")  # skip the header and end
         else:
             raise JVCCommunicationError("Unexpected response for get_model()")
 
@@ -236,7 +238,6 @@ class JVCProjectorClient:
         """Check if the lamp is fully powered on."""
         return asyncio.run(self.async_is_lamp_on())
 
-
     def is_on(self) -> bool:
         """Check if the projector is powered on."""
         return asyncio.run(self.async_is_on())
@@ -244,7 +245,9 @@ class JVCProjectorClient:
     async def async_get_input(self) -> dict[str, str]:
         """Get the projector's active hdmi input and if it has an active signal."""
         if not await self.async_is_lamp_on():
-            raise JVCPoweredOffError("Can't get input, the projector is powered off or starting up.")
+            raise JVCPoweredOffError(
+                "Can't get input, the projector is powered off or starting up."
+            )
         inp = await self._async_send_command(Commands.current_input.value)
         sig = await self._async_send_command(Commands.signal_active.value)
         return {"input": Responses(inp).name, "signal": Responses(sig).name}
@@ -254,40 +257,31 @@ class JVCProjectorClient:
         return asyncio.run(self.async_get_input())
 
 
-
 class JVCCannotConnectError(Exception):
     """Exception when we can't connect to the projector"""
+
     pass
+
 
 class JVCHandshakeError(Exception):
     """Exception when there was a problem with the 3 step handshake"""
+
     pass
+
 
 class JVCCommunicationError(Exception):
     """Exception when there was a communication issue"""
+
     pass
+
 
 class JVCCommandNotFoundError(Exception):
     """Exception when the requested command doesn't exist"""
+
     pass
+
 
 class JVCPoweredOffError(Exception):
     """Exception when projector is powered off and can't accept some commands."""
+
     pass
-
-# async def coro(p):
-#     a = await p.async_get_mac()
-#     print(a)
-
-# async def main():
-#     p = JVCProjectorClient("192.168.1.14", delay_seconds=0.6)
-#     #limiter = AsyncLimiter(1, 1.5)
-#     while True:
-#         #async with limiter:
-#         #    if limiter.has_capacity(): print("has cap")
-#         await coro(p)
-
-
-
-# if __name__=="__main__":
-#     asyncio.run(main())
